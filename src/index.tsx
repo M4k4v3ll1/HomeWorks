5dc475bca4b4cefbca106d459dff90a7f508317f
M4k4v3ll1 <141486338+M4k4v3ll1@users.noreply.github.com>
2024-02-03 20:37:58 +0400

    S3_HW1 is done. Project has been corrected on all points, including design (except moving of main page when sidebar is opening)

--- src/index.tsx
+++ src/index.tsx
@@ -4,17 +4,12 @@ import './index.css'
 import App from './s1-main/App'
 import reportWebVitals from './reportWebVitals'
 //import store from './s2-homeworks/hw10/bll/store'
-import {Provider} from "react-redux";
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-    <React.StrictMode>
-        {/*для дз 10*/}
-        {/*<Provider store={store}>*/}
-            <App/>
-        {/*</Provider>*/}
-    </React.StrictMode>
+    <App/>
 )
 
 // If you want to start measuring performance in your app, pass a function
