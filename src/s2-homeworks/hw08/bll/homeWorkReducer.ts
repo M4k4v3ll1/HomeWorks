9a0cd71d7984581bb96a0c78f41387cd8627bc17
Ivan Trandin <trandinii@gmail.com>
2024-02-14 16:16:09 +0400

    S3_HW3 almost done (except checkbox visibility) and S3_HW4 is done

---
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -0,0 +1,22 @@
+import {UserType} from '../HW8'
+
+type ActionType =
+    | { type: 'sort'; payload: 'up' | 'down'}
+    | { type: 'check'; payload: number }
+
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[]=> { // need to fix any
+    switch (action.type) {
+        case 'sort': { // by name
+            if (action.payload === "up") {
+                return [...state.sort((a, b) => a.name.localeCompare(b.name))]
+            } else if (action.payload === "down") {
+                return [...state.sort((a, b) => b.name.localeCompare(a.name))]
+            } else return state
+        }
+        case 'check': {
+            return state.filter(s => s.age >= action.payload)
+        }
+        default:
+            return state
+    }
+}
